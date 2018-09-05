3590e22f14626214f02921e4522320c7b2df30fb
wittersa <witters.amand@gmail.com>
2018-09-05 08:51:34 +0200

    feat(ClassicSetup): init

---
+++ config/webpack.dev.js
@@ -0,0 +1,15 @@
+const path = require("path");
+
+module.exports = {
+    entry: {
+        main: "./src/main.js"
+    },
+    mode: "development",
+    output: {
+        filename: "[name]-bundle.js",
+        path: path.resolve(__dirname, "../dist")
+    },
+    devServer: {
+        contentBase: "dist"
+    }
+};
